fb4655aa42894f984901493e2e0c656b42900e9a
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2026-08-13 22:34:38 +0200

    Preserve full relabel test coverage

--- tests/test_relabel.py
+++ tests/test_relabel.py
@@ -20,44 +20,84 @@ def test_first_occurrence_order():
     assert n == 3
 
 
-def test_offset_and_in_place():
+def test_offset():
     x = np.array([0, 9, 9, 20], dtype=np.uint32)
     y, n = relabel_sequential(x, offset=100)
     np.testing.assert_array_equal(y, [0, 101, 101, 102])
     assert n == 2
-    z = np.array([0, 9, 20], dtype=np.uint32)
-    out, n = relabel_sequential(z, in_place=True)
-    assert out is z
-    np.testing.assert_array_equal(z, [0, 1, 2])
+
+
+def test_in_place_identity():
+    x = np.array([0, 9, 20], dtype=np.uint32)
+    y, n = relabel_sequential(x, in_place=True)
+    assert y is x
+    np.testing.assert_array_equal(x, [0, 1, 2])
     assert n == 2
 
 
-def test_non_contiguous_and_bad_dtype():
+def test_non_contiguous_in_place_rejected():
     x = np.arange(16, dtype=np.uint32).reshape(4, 4)[:, ::2]
     with pytest.raises(ValueError):
         relabel_sequential(x, in_place=True)
-    y, _ = relabel_sequential(x)
+
+
+def test_non_contiguous_copy_allowed():
+    x = np.array([[0, 4, 0, 7], [7, 4, 9, 9]], dtype=np.uint32)[:, ::2]
+    y, n = relabel_sequential(x)
     assert y.flags.c_contiguous
+    assert n == 2
+
+
+def test_bad_dtype():
     with pytest.raises(TypeError):
         relabel_sequential(np.array([0, 1], dtype=np.int32))
 
 
 @pytest.mark.parametrize("dtype", [np.uint32, np.uint64])
-def test_offset_capacity(dtype):
-    max_label = int(np.iinfo(dtype).max)
-    empty = np.zeros(4, dtype=dtype)
-    y, n = relabel_sequential(empty, offset=max_label)
-    np.testing.assert_array_equal(y, empty)
+def test_max_offset_rejects_new_foreground(dtype):
+    x = np.array([1], dtype=dtype)
+    with pytest.raises(OverflowError):
+        relabel_sequential(x, offset=np.iinfo(dtype).max)
+
+
+@pytest.mark.parametrize("dtype", [np.uint32, np.uint64])
+def test_max_offset_empty_foreground_is_ok(dtype):
+    x = np.zeros(4, dtype=dtype)
+    y, n = relabel_sequential(x, offset=np.iinfo(dtype).max)
+    np.testing.assert_array_equal(y, x)
     assert n == 0
+
+
+@pytest.mark.parametrize("dtype", [np.uint32, np.uint64])
+def test_exact_remaining_label_capacity_succeeds(dtype):
+    max_label = int(np.iinfo(dtype).max)
     x = np.array([0, 50, 10, 50, 999], dtype=dtype)
     y, n = relabel_sequential(x, offset=max_label - 3)
-    np.testing.assert_array_equal(y, np.array([0, max_label - 2, max_label - 1, max_label - 2, max_label], dtype=dtype))
+    np.testing.assert_array_equal(
+        y,
+        np.array([0, max_label - 2, max_label - 1, max_label - 2, max_label], dtype=dtype),
+    )
     assert n == 3
+
+
+@pytest.mark.parametrize("dtype", [np.uint32, np.uint64])
+def test_one_label_beyond_remaining_capacity_raises(dtype):
+    max_label = int(np.iinfo(dtype).max)
+    x = np.array([0, 50, 10, 999], dtype=dtype)
     with pytest.raises(OverflowError):
-        relabel_sequential(np.array([1], dtype=dtype), offset=max_label)
+        relabel_sequential(x, offset=max_label - 2)
 
 
-def _reference(x, offset=0):
+@pytest.mark.parametrize("dtype", [np.uint32, np.uint64])
+def test_existing_label_at_dtype_limit_does_not_overflow(dtype):
+    max_label = int(np.iinfo(dtype).max)
+    x = np.array([7, 7, 0, 7], dtype=dtype)
+    y, n = relabel_sequential(x, offset=max_label - 1)
+    np.testing.assert_array_equal(y, np.array([max_label, max_label, 0, max_label], dtype=dtype))
+    assert n == 1
+
+
+def _reference_first_occurrence(x, offset=0):
     out = np.empty_like(x)
     mapping = {}
     next_id = offset
@@ -79,7 +119,7 @@ def test_randomized_matches_reference(dtype):
     for _ in range(25):
         x = rng.integers(0, 5000, size=(37, 53), dtype=dtype)
         x[x % 7 == 0] = 0
-        expected, expected_n = _reference(x, offset=17)
+        expected, expected_n = _reference_first_occurrence(x, offset=17)
         got, n = relabel_sequential(x, offset=17)
         np.testing.assert_array_equal(got, expected)
         assert n == expected_n
